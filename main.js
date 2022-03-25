4a4c81dbc2cb3a0bef539effe43d26431d633762
Bee-Stack <102287027+Bee-Stack@users.noreply.github.com>
2022-03-25 18:41:21 +0530

    Updates to main.js
    Use template literals

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const user = document.querySelector('#user');
 const greeting = document.querySelector('#greeting');
 
 user.addEventListener('keyup', (evt) => {
+  evt.preventDefault();
   if (evt.key === 'Enter') {
     if (evt.target.value !== '') {
-      greeting.innerText = 'Hey, ' + evt.target.value + '!';
+      greeting.innerText = `Hey, ${evt.target.name}!`;
     }
   }
 });
